6784fef78fee0bc8a7bfc0b8ac964c4e1e7fc30b
m0u5 <the1337m0u53@gmail.com>
2023-05-25 15:39:59 +0300

    Добавлен публичный метод для второго дз

--- WordsFinder/WordsFinder.cs
+++ WordsFinder/WordsFinder.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace WordsFinder
 {
@@ -16,7 +17,7 @@ namespace WordsFinder
 
         private Dictionary<string, int> CountWords(string text)
         {
-            text = text;
+             
             foreach (Match match in regex.Matches(text))
             {
                 string Word = match.Value;
@@ -36,6 +37,32 @@ namespace WordsFinder
             _result = _words.AsParallel().OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
             return _result;
         }
-        
+        public  Dictionary<string, int> CountWordsPublic(string text)
+        {
+            List<Task> TasksList = new List<Task>();
+            
+            foreach (Match match in regex.Matches(text))
+            {
+                TasksList.Add(Task.Factory.StartNew(() =>
+                {
+                    string Word = match.Value;
+                    Word = Word.ToLower();
+                    lock (_words)
+                    {
+                        if (_words.ContainsKey(Word))
+                        {
+                            _words[Word]++;
+                        }
+                        else { _words.Add(Word, 1); }
+                    }
+                }));
+
+            }
+            Task.WaitAll(TasksList.ToArray());
+            SortWordsList();
+            return _result;
+
+        }
+
     }
 }
